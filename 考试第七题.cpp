5936e0692c3b544cb0c440530f041b3010a156c0
Laiakuai <407593236@qq.com>
2020-02-04 16:06:08 +0800

    哈哈哈哈哈

---
+++ 考试第七题.cpp
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+int main() {
+	int a, b, c=0;
+    char ch;
+	scanf("%d", &a);
+	scanf("%c", &ch);
+	while (ch != '='&&c!=1) {
+		scanf("%d", &b);
+		switch (ch) {
+		case'+':a += b;
+			break;
+		case'-':a -= b; 
+			break;
+		case'*':a *= b; 
+			break;
+		case'/':if (b ==0)
+			c = 1;
+				else a /= b;
+			break;
+		default:c = 1;
+		}
+		scanf("%c", &ch);
+	}
+	if (c == 1)
+		printf("ERROR");
+	else
+		printf("%d", a);
+	return 0;
+}
