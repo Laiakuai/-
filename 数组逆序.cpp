c0b839c1e158d49929c3ac886d8ce103fd1c8e84
Laiakuai <407593236@qq.com>
2020-02-04 15:59:19 +0800

    haha

---
+++ 数组逆序.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main() {
+	int a[10], b[10], c, d, n, e = 0;
+	scanf("%d", &n);
+	for (c = 0; c < n; c++) {
+		scanf("%d", &a[c]);
+	}
+	for (d = n - 1; d >= 0; d--) {
+		b[d] = a[e];
+		e++;
+	}
+	printf("%d", b[0]);
+	for (c = 1; c < n; c++) {
+		printf(" %d", b[c]);
+	}
+	return 0;
+}
